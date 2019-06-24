5425edcb3dcaa1f0db479d8533ad5c06f2e83bec
Ubuntu <ubuntu@ip-172-31-90-167.ec2.internal>
2019-06-24 13:16:05 +0000

    pie chart good start

--- static/js/graphs.js
+++ static/js/graphs.js
@@ -3,5 +3,18 @@ queue()
     .await(makeGraphs);
     
 function makeGraphs(error, payrollData) {
+    var ndx = crossfilter(payrollData);
+    
+    var name_dim = ndx.dimension(dc.pluck('Type'));
+    var total_cost_pie_chart =name_dim.group().reduceSum(dc.pluck('Sum'));
+    
+    dc.pieChart('#total-cost-pie-chart')
+        .height(330)
+        .radius(90)
+        .transitionDuration(1500)
+        .dimension(name_dim)
+        .group(total_cost_pie_chart);
+        
+    dc.renderAll();
 
 }
