7cb25d534e46a1965ee66a80f16aba74d152f3a5
Ubuntu <ubuntu@ip-172-31-90-167.ec2.internal>
2019-07-11 14:59:49 +0000

    version to push to git hub for help with dimension for pie chart

--- static/js/graphs.js
+++ static/js/graphs.js
@@ -6,15 +6,28 @@ queue()
 /*skapar graf och hämtar data*/
 function makeGraphs(error, payrollData) {
     var ndx = crossfilter(payrollData);
-    var type_dim  = ndx.dimension(function (d) {return ((d["Type"] = "Taxes") && (d["Type"] = "Nettpay")) ;});
+    var type_dim  = ndx.dimension(function (d) {
+        return ((d["Type"] = "Taxes") 
+            && (d["Type"] = "Nettpay")
+            && (d["Type"] = "Social Security Fees"));
+    });
+    var total_cost_pie_chart = type_dim.group().reduceSum(dc.pluck('Sum'));
     
-    /*var type_dim = ndx.dimension(function(d) { return d["Type"] = "Taxes"} &&  );
+dc.pieChart('#total-cost-pie-chart')
+        .height(330)
+        .radius(90)
+        .transitionDuration(1500)
+        .dimension(type_dim)
+        .group(total_cost_pie_chart);
+        
+    dc.renderAll();
+
+}
+    
+     /*var type_dim = ndx.dimension(function(d) { return d["Type"] = "Taxes"} &&  );
     /*varför inte skapa en dimension för varje av de 3 jag vill ha? type=xx */
     
     /*var type_dim_filter = function(type_dim){return d.Type = if };*/
-    
-    
-    var total_cost_pie_chart = type_dim.group().reduceSum(dc.pluck('Sum'));
     
     /* var payroll cost. för varje cell i excellen sparas som var payroll cost. den avriabeln stämms sedan av mot de data jag vill ha
     var payroll_cost;
@@ -27,13 +40,4 @@ function makeGraphs(error, payrollData) {
      tror att dc.pluck('Sum')} ovan är fel. måste addera ihopp summorna på annat sätt*/
 
     
-    dc.pieChart('#total-cost-pie-chart')
-        .height(330)
-        .radius(90)
-        .transitionDuration(1500)
-        .dimension(type_dim)
-        .group(total_cost_pie_chart);
-        
-    dc.renderAll();
-
-}
+    
