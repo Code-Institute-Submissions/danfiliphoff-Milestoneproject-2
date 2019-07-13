7fdf71169d22b7bf91ad9daa5af8b445bae77fde
Ubuntu <ubuntu@ip-172-31-90-167.ec2.internal>
2019-07-13 15:52:14 +0000

    total cost pie chart done (needs minor cosmetic fixes)

--- static/js/graphs.js
+++ static/js/graphs.js
@@ -1,25 +1,23 @@
 /*väntar med att skapa graf tills data är laddat*/
 queue()
-    .defer(d3.csv, "data/payroll.data.csv")
+    .defer(d3.csv, "data/total.cost.csv")
     .await(makeGraphs);
     
 /*skapar graf och hämtar data*/
-function makeGraphs(error, payrollData) {
-    var ndx = crossfilter(payrollData);
+function makeGraphs(error, totalCost) {
+    var ndx = crossfilter(totalCost);
     var type_dim = ndx.dimension(function(d) { return d["Type"]; });
     var total_cost_pie_chart = type_dim.group().reduceSum(dc.pluck('Sum'));
     
-    /*for ( of )
-    if
-    else if
-    else if*/
     
     dc.pieChart('#total-cost-pie-chart')
-        .height(330)
-        .radius(90)
+        .height(500)
+        .radius(450)
         .transitionDuration(1500)
         .dimension(type_dim)
         .group(total_cost_pie_chart);
+        .label()
+        
         
     dc.renderAll();
 
