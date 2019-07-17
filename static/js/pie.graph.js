e221fb5ba558f76189d68b6d9fe0bad3b6bee905
Ubuntu <ubuntu@ip-172-31-90-167.ec2.internal>
2019-07-17 16:25:22 +0000

    pie.graph.js structure changed to accomodate more then one chart

--- static/js/pie.graph.js
+++ static/js/pie.graph.js
@@ -1,16 +1,25 @@
+/*steg: 
+1. skapa HTML.
+2. skapa function
+3.lägg till function under make graph*/
+
 /*väntar med att skapa graf tills data är laddat*/
 queue()
     .defer(d3.csv, "data/total.cost.csv")
-    .await(makePie);
+    .await(makeGraphs);
+    
+function makeGraphs(error, totalCost){
+   var ndx=crossfilter(totalCost);
+   makePie(ndx);
+   dc.renderAll();
+}
+    
     
 /*skapar graf och hämtar data*/
-function makePie(error, totalCost) {
-    var pie = crossfilter(totalCost);
-    var type_dim = pie.dimension(function(d) { return d["Type"]; });
+function makePie(ndx) {
+    var type_dim = ndx.dimension(function(d) { return d["Type"]; });
     var total_cost_pie_chart = type_dim.group().reduceSum(dc.pluck('Sum'));
 
-    
-    
     dc.pieChart('#total-cost-pie-chart')
         .height(1000)
         .radius(200)
@@ -24,9 +33,4 @@ function makePie(error, totalCost) {
         */
         .label(function(d){return d.key + " " + d.value + " " + "KR (" +  Math.round((d.value/(8129071/100))*100)/100 + "%)"})
         .renderLabel(true);
-    
-        
-        
-    dc.renderAll();
-
 }
