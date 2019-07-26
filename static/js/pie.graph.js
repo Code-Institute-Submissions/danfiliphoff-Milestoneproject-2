d582a3f1e3548a5e441695a47ddf3fe307a7ed08
Ubuntu <ubuntu@ip-172-31-90-167.ec2.internal>
2019-07-26 14:54:05 +0000

    continued issues with parsing dates

--- static/js/pie.graph.js
+++ static/js/pie.graph.js
@@ -9,8 +9,26 @@ queue()
     .await(makeGraphs);
     
 function makeGraphs(error, totalCost){
+    
+    var dateFormatSpecifier = "%Y-%m-%d"; 
+    var dateFormat = d3.timeFormat(dateFormatSpecifier); 
+    var dateFormatParser = d3.timeParse(dateFormatSpecifier); 
+    var numberFormat = d3.format('.2f'); 
+
+    totalCost.forEach(function(d) { 
+        d.Sum = parseInt(d.Sum, 10); /*intiger vs string*/ 
+        d.dd = dateFormatParser(d.date); 
+        d.month = d3.timeMonth(d.dd); 
+        d.close = +d.close; 
+        d.open = +d.open; 
+
+    }); 
+
+    console.log(dateFormatParser); 
+    
    var ndx=crossfilter(totalCost);
    makePie(ndx);
+   makeTotalLineGraph(ndx);
    dc.renderAll();
 }
     
@@ -34,3 +52,18 @@ function makePie(ndx) {
         .label(function(d){return d.key + " " + d.value + " " + "KR (" +  Math.round((d.value/(8129071/100))*100)/100 + "%)"})
         .renderLabel(true);
 }
+
+function makeTotalLineGraph(ndx) {  
+    var payment_date_dim = ndx.dimension(function(d) { return d["PaymentDate"]; });  
+    var total_spend_per_date = payment_date_dim.group().reduceSum(dc.pluck('Sum'));  
+  
+    dc.TotalLineGraph('#total-cost-line-graph')  
+        .width(1000)  
+        .height(300)  
+        .dimension(payment_date_dim)  
+        .group(total_spend_per_date)  
+        .transitionDuration(500)  
+        .x(d3.time.scale().domain([minDate,maxDate]))  
+        .xAxisLabel("Month")  
+        .yAxis().ticks(4);  
+}  
