88f16b925e949b65b375ad8e349708063e5bf441
danfiliphoff <danfiliphoff@hotmail.com>
2019-09-22 04:22:13 +0000

    account.html. most ids removed and replaced with class

--- static/js/account.js
+++ static/js/account.js
@@ -1,9 +1,9 @@
 queue()
     .defer(d3.csv, "data/account.csv")
     .await(makeAccountDataGraphs);
-    
+
 function makeAccountDataGraphs(error, account){
-    
+
     var parseDate = d3.time.format("%Y-%m-%d").parse;
     account.forEach(function(d){
         d.PaymentDate = parseDate(d.PaymentDate);
@@ -13,11 +13,11 @@ function makeAccountDataGraphs(error, account){
     account.forEach(function(d){
         d.Account = parseInt(d.Account, 10);
     });
-    
+
     account.forEach(function(d){
         d.Sum = parseInt(d.Sum, 10);
     });
-    
+
    var ndx=crossfilter(account);
    salary_type_selector(ndx);
    cost_per_account(ndx);
@@ -29,28 +29,28 @@ function makeAccountDataGraphs(error, account){
 function salary_type_selector(ndx){
     var salary_type_selector_dim = ndx.dimension(dc.pluck('Type'));
     var salary_type_selector_group = salary_type_selector_dim.group();
-    
-    dc.selectMenu("#salary-type-selector")
+
+    dc.selectMenu(".salary-type-selector")
         .dimension(salary_type_selector_dim)
         .group(salary_type_selector_group);
 }
 
 function cost_over_time(ndx) {
-    var cost_over_time_dim = ndx.dimension(dc.pluck("PaymentDate"));  
+    var cost_over_time_dim = ndx.dimension(dc.pluck("PaymentDate"));
     var cost_over_time_group = cost_over_time_dim.group().reduceSum(dc.pluck('Sum'));
-    
+
     var minDate = cost_over_time_dim.bottom(1)[0].PaymentDate;
     var maxDate = cost_over_time_dim.top(1)[0].PaymentDate;
-  
-    dc.lineChart('#Cost-Over-Time')  
-        .width(1000)  
-        .height(500)  
+
+    dc.lineChart('.Cost-Over-Time')
+        .width(1000)
+        .height(500)
         .useViewBoxResizing(true)
         .margins({top: 10, right: 150, bottom: 30, left: 150})
-        .dimension(cost_over_time_dim)  
+        .dimension(cost_over_time_dim)
         .elasticY(true)
-        .group(cost_over_time_group)  
-        .transitionDuration(500)  
+        .group(cost_over_time_group)
+        .transitionDuration(500)
         .x(d3.time.scale().domain([minDate,maxDate]))
         .yAxis().ticks(15);
         }
@@ -59,7 +59,7 @@ function cost_per_account(ndx) {
     var cost_per_account_dim = ndx.dimension(dc.pluck("Account"));
     var cost_per_account_group = cost_per_account_dim.group().reduceSum(dc.pluck('Sum'));
 
-    dc.pieChart('#cost-per-account')
+    dc.pieChart('.cost-per-account')
         .height(500)
         .width(500)
         .useViewBoxResizing(true)
@@ -76,7 +76,7 @@ function cost_per_account(ndx) {
             .gap(9)
             .legendText(
                 function(d) {
-                    return d.name + " - " + d.data + " " + "KR (" 
+                    return d.name + " - " + d.data + " " + "KR ("
             +  Math.round((d.data/(10241617/100))*100)/100 + "%)"})
             )
         .renderLabel(false);
@@ -86,7 +86,7 @@ function cost_per_type(ndx){
     var cost_per_type_dimension = ndx.dimension(dc.pluck("Type"));
     var cost_per_type_group = cost_per_type_dimension.group().reduceSum(dc.pluck('Sum'));
     var filtered_cost_per_type_group = remove_empty_bins(cost_per_type_group);
-    
+
     function remove_empty_bins(cost_per_type_group) {
     return {
         all:function () {
@@ -96,8 +96,8 @@ function cost_per_type(ndx){
             }
            };
           }
-    
-     dc.barChart('#Cost-Per-Type')
+
+     dc.barChart('.Cost-Per-Type')
             .width(1700)
             .height(700)
             .useViewBoxResizing(true)
