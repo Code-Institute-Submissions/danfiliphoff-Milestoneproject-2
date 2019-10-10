eabe50fe81d5c4f7b8680850ec15b9985fc3d4e8
danfiliphoff <danfiliphoff@hotmail.com>
2019-10-10 19:25:42 +0000

    margin on total cost bar chart adjusted so that whole label is displayed

--- static/js/total_cost.js
+++ static/js/total_cost.js
@@ -91,7 +91,7 @@ function StackedBarChartTotalCost(ndx){
             .width(1500)
             .height(800)
             .useViewBoxResizing(true)
-            .margins({top: 10, left: 80, right: 1, bottom: 50})
+            .margins({top: 10, left: 95, right: 1, bottom: 50})
             .dimension(payment_date_dim_bar)
             .transitionDuration(500)
             .group(NettpayByMonth, "Nettpay")
